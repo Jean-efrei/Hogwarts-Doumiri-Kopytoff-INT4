748ceb8c7a6f8575d1e576e2ece90afa5b20d38e
Iman <imanmona09@icloud.com>
2025-11-27 19:05:53 +0100

    adding newFile

---
+++ utils/input_utils.py
@@ -0,0 +1,6 @@
+def ask_text(message):
+    name = ask_text("Enter yout character's name: ")
+    return name
+
+def ask_number(message, min_val=None, max_val=None):
+    num= ask_number("Enter an integer: ")
