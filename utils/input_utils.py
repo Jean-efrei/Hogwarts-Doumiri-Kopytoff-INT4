3b40a902744afb9020d14b775a398cbc4abcea15
Iman <imanmona09@icloud.com>
2025-12-02 09:49:38 +0100

    adding newFile

--- utils/input_utils.py
+++ utils/input_utils.py
@@ -1,6 +1,38 @@
 def ask_text(message):
-    name = ask_text("Enter yout character's name: ")
-    return name
+    question= input("Enter your character's name: ")
+    return question
 
 def ask_number(message, min_val=None, max_val=None):
     num= ask_number("Enter an integer: ")
+    if min_val is not None and max_val is not None:
+        if num < min_val or num > max_val:
+            return("Please enter a number between {} and {}")
+        else:
+            return message , num
+
+def ask_choice(message, options):
+    choice = input("Do you want to continue ? ")
+    option_1 = input("1. Yes")
+    option_2 = input("2. No")
+    if choice == option_1:
+        return option_1
+    elif choice == option_2:
+        return option_2
+    else:
+        return("Please enter a valid option")
+
+def load_file(file_path):
+    with open(file_path) as f:
+        return f.read()
+
+
+
+
+
+
+
+
+
+
+
+
