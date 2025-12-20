8121d6ffef2d16610c2cdf964fe6943496c06c34
jeank <jean.kopytoff@efrei.net>
2025-12-20 16:22:19 +0100

    chapter 1 updates

--- receive_letter.py
+++ receive_letter.py
@@ -1,3 +1,6 @@
+from utils.input_utils import ask_choice
+
+
 def receive_letter(character):
     print("An owl flies through the window, delivering a letter sealed with the Hogwarts crest...")
     print("Dear Mr Potter, "
@@ -9,10 +12,10 @@ def receive_letter(character):
     print("Do you accept this invitation and go to Hogwarts ?")
     print("1 : Yes of course")
     print("2 : No, what is that weird letter, Wizards don't exist")
-    x = int(input("Your choice:"))
-    if x == 1:
+    x = ask_choice("Your choice:", ["1", "2"])
+    if x == "1":
         print("A strange feeling browses your entire body, it's the beginning of a great adventure. ")
-        if x == 2:
-            print("You tear up your letter, and throw it into the fire place, Uncle Vernon cheers : Finally someone normal in this house !")
-            print("The magical world will never hear about, what a wast of magic ! GAME OVER")
+    else:
+        print("You tear up your letter, and throw it into the fire place, Uncle Vernon cheers : Finally someone normal in this house !")
+        print("The magical world will never hear about, what a wast of magic ! GAME OVER")
 
