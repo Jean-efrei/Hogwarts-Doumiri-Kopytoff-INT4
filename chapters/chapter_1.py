31678e8e350466292365944aaf3859f4fa63f7dd
jeank <jean.kopytoff@efrei.net>
2025-12-21 19:32:48 +0100

    chapter 1 and 2 updates

--- chapters/chapter_1.py
+++ chapters/chapter_1.py
@@ -37,21 +37,25 @@ if __name__ == "__main__":
     introduction()
     hero = create_character()
 
-def receive_letter():
 
+def receive_letter(character):
     print("An owl flies through the window, delivering a letter sealed with the Hogwarts crest...")
-    print("“Dear Student,")
-    print("We are pleased to inform you that you have been accepted to Hogwarts")
-    print("School of Witchcraft and Wizardry!”\n")
-
-    choice = ask_choice("Do you accept this invitation and go to Hogwarts?",["Yes, of course!", "No, I'd rather stay with Uncle Vernon..."])
-
-    if choice == "No, I'd rather stay with Uncle Vernon...":
-        print("You tear up the letter, and Uncle Vernon cheers:")
-        print("“EXCELLENT! Finally, someone NORMAL in this house!”")
-        print("The magical world will never know you existed... Game over.")
-        exit(0)
-    print("\nYou clutch the letter tightly. Your life will never be the same again...")
+    print("Dear Mr Potter, "
+          "We are pleased to inform you that you have been accepted at Hogwarts School of Witchcraft and Wizardry."
+          "Please find enclosed a list of all necessary books and equipment."
+          "Term begins on 1 September. We await your owl by no later than 31 July."
+          "Yours sincerely,"
+          "Minerva McGonagall")
+    print("Do you accept this invitation and go to Hogwarts ?")
+    print("1 : Yes of course")
+    print("2 : No, what is that weird letter, Wizards don't exist")
+    x = ask_choice("Your choice:", ["1", "2"])
+    if x == "1":
+        print("A strange feeling browses your entire body, it's the beginning of a great adventure. ")
+    else:
+        print(
+            "You tear up your letter, and throw it into the fire place, Uncle Vernon cheers : Finally someone normal in this house !")
+        print("The magical world will never hear about, what a waste of magic ! GAME OVER")
 
 
 def meet_hagrid(character) :
--- chapters/chapter_2.py
+++ chapters/chapter_2.py
@@ -19,8 +19,10 @@ def meet_friends(character):
     x = ask_choice ("Your choice:", ["1", "2"])
     if x == "1":
         print("Ron smiles: — Awesome! You'll see, Hogwarts is amazing!")
+        character["Attributes"]["Loyalty"] += 1
     else:
         print("Okay, I'll get another seat, see you !")
+        character["Attributes"]["Ambition"] += 1
     print("A girl enters next, already carrying a stack of books.")
     print("Hello, I'm Hermione Granger. Have you ever read 'A History of Magic'?")
     print("How do you respond?")
@@ -29,8 +31,10 @@ def meet_friends(character):
     y = ask_choice("Your choice:", ["1", "2"])
     if y == "1" :
         print("Hermione smiles, impressed: — Oh, that's rare! You must be very clever!")
+        character["Attributes"]["Intelligence"] += 1
     else:
         print("Seriously ? You looked smarter when I saw you by the window. Do you really think you can become an Auror without books ?")
+        character["Attributes"]["Courage"] += 1
     print("Then a blonde boy enters, looking arrogant.")
     print("I'm Draco Malfoy. It's best to choose your friends carefully from the start, don't you think?")
     print("How do you respond?")
@@ -40,12 +44,15 @@ def meet_friends(character):
     z = ask_choice("Your choice:", ["1", "2", "3"])
     if z == "1":
         print("I hope we'll be together in Slytherin !")
+        character["Attributes"]["Ambition"] += 1
     elif z == "2":
         print("Draco frowns, annoyed. — You'll regret that!")
+        character["Attributes"]["Loyalty"] += 1
     else:
         print("HOW DARE YOU ? DO YOU KNOW WHO IS MY FATHER ?")
     print("The train continues its journey. Hogwarts Castle appears on the horizon...")
     print ("Your choices already say a lot about your personality!")
+    character["Attributes"]["Courage"] += 1
 
 
 def sorting_ceremony(character):
@@ -67,27 +74,10 @@ def sorting_ceremony(character):
         )
     ]
     print("The sorting ceremony begins in the Great Hall... The Sorting Hat observes you for a long time before asking its questions:")
-    print(questions[0][0])
-    print("1 :",questions[0][1][0])
-    print("2 :",questions[0][1][1])
-    print("3 :",questions[0][1][2])
-    print("4 :",questions[0][1][3])
-    x = int(input("Your choice :"))
-    print(questions[1][0])
-    print("1 :",questions[1][1][0])
-    print("2 :",questions[1][1][1])
-    print("3 :",questions[1][1][2])
-    print("4 :",questions[1][1][3])
-    y = int(input("Your choice :"))
-    print(questions[2][0])
-    print("1 :",questions[2][1][0])
-    print("2 :",questions[2][1][1])
-    print("3 :",questions[2][1][2])
-    print("4 :",questions[2][1][3])
-    z = int(input("Your choice :"))
+    return assign_house(character, questions)
 
 def enter_common_room(character):
-    my_house = sorting_ceremony
+    my_house = sorting_ceremony(character)
     house = load_file("data/houses.json")
     print("You follow the prefects through the castle corridors...")
     print(house[my_house]["emoji"], " ",house[my_house]["description"])
